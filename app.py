0d37a01b2d9995ba78dd0971ffcfd6f30b567119
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2025-06-06 23:15:41 -0400

    base html done I think

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html", title="Home", alt_home="terminal")
+
+@app.route("/terminal")
+def terminal():
+    return render_template("terminal.html", title="Terminal", alt_home="home")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html", title="Projects")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html", title="Blog")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="Contact")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html", title="Resume")
+
+if __name__ == "__main__":
+    app.run(debug=True)
