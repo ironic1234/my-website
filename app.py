cc7b309c56b0d5520da40f504a289b72b3cb6fce
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2025-07-08 17:10:56 -0400

    fixed routes and freeze

--- app.py
+++ app.py
@@ -6,21 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", title="Home", alt_home="terminal")
 
-@app.route("/terminal")
+@app.route("/terminal/")
 def terminal():
     return render_template("terminal.html", title="Terminal", alt_home="home")
 
-@app.route("/projects")
+@app.route("/projects/")
 def projects():
     return render_template("projects.html", title="Projects", alt_home="home")
 
-@app.route("/blog")
+@app.route("/blog/")
 def blog():
     return render_template("blog/index.html", title="Blog", alt_home="home")
-
-@app.route("/blog/<path:post>")
-def blog_post(post):
-    return render_template(f"blog/{post}.html", title=post.replace("-", " ").title(), alt_home="home")
 
 if __name__ == "__main__":
     app.run(debug=True)
